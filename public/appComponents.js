a966ada7ba3bd6c9653991bf01295ac140645ed8
Shmuel Kallner <kallner@il.ibm.com>
2016-11-27 18:11:26 +0200

    IBM-Swift/Kitura#33 Handle multi-line messages

--- public/appComponents.js
+++ public/appComponents.js
@@ -62,10 +62,12 @@ var setupWebSocketClient = function($scope) {
                     break;
 
                 case 'M':
+                    var messageText = parts[2];
+                    messageText = messageText.replace(/\n/g, '<br>');
                     var snippet =
                         '<div class="messageLine">' +
                           '<div class="messageDisplayName">' + parts[1] + '</div>' +
-                          '<div class="messageText">' + parts[2] + '</div>' +
+                          '<div class="messageText">' + messageText + '</div>' +
                         '</div>';
                     var messagesArea = $('.messagesArea');
                     messagesArea.html(messagesArea.html() + snippet);
