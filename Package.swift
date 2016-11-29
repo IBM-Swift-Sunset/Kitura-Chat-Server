b3570417b8f571048c5ae3f400c1c988121fd647
Shmuel Kallner <kallner@il.ibm.com>
2016-11-29 13:44:46 +0200

    IBM-Swift/Kitura#33 Upgraded Kitura-WebSocket dependency

--- Package.swift
+++ Package.swift
@@ -21,6 +21,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 2),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-WebSocket", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/IBM-Swift/Kitura-WebSocket", majorVersion: 0, minor: 2)
     ]
 )
