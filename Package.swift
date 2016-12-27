9a772ab6cdb820705e731c1e6d819605fcda842c
Shmuel Kallner <kallner@il.ibm.com>
2016-12-27 19:51:00 +0200

    IBM-Swift/Kitura#33 Updated dependencies

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ import PackageDescription
 let package = Package(
     name: "KituraChatServer",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 3),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 3),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-WebSocket", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 4),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-WebSocket", majorVersion: 0, minor: 3)
     ]
 )
